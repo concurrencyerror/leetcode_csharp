ca3dd9d98a57b9343c7ff86d345ae341a9b2b9aa
horce1024@gmail.com <horce1024@gmail.com>
2022-01-13 09:26:24 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode
+{
+    class Program
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("hello,dotnet6");
+        }
+    }
+}
