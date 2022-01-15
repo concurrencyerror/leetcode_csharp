a08fcbf4ecbfb25c5e10b059f96864a3d2fdcc03
horce1024@gmail.com <horce1024@gmail.com>
2022-01-15 12:58:43 +0800

    今天的每日一题

---
+++ TotalMoney1716.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode
+{
+    internal class TotalMoney1716
+    {
+        public int totalMoney(int n)
+        {
+            int num = n / 7;
+            int many = n % 7;
+            int last = 0;
+            int begin = num + 1;
+            for (int i = 0; i < many; i++)
+            {
+                last += begin;
+                begin++;
+            }
+            int sub = 0;
+            for (int i = 0; i < num; i++)
+            {
+                sub += i * 7;
+            }
+            return (1 + 7) * 7 / 2 * num + sub + last;
+        }
+    }
+}
