188d590fc375e1d9f4b19d0a9ee5f6c50e468f50
horace <horce1024@gmail.com>
2022-11-16 13:12:16 +0800

    每日一题，也开始用c#来写了

---
+++ Solution1710.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    internal class Solution1710
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            int result = 0;
+            Dictionary<int, int> dictionary = new();
+            foreach (var item in boxTypes)
+            {
+                if (dictionary.ContainsKey(item[1]))
+                {
+                    dictionary[item[1]] = dictionary[item[1]] + item[0];
+                }
+                else
+                {
+                    dictionary.Add(item[1], item[0]);
+                }
+            }
+            foreach (var item in from dic in dictionary orderby dic.Key descending select dic)
+            {
+                if (truckSize > item.Value)
+                {
+                    result += item.Value * item.Key;
+                    truckSize -= item.Value;
+                }
+                else
+                {
+                    result += truckSize * item.Key;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
