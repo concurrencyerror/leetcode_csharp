3332b115b0e1b56e6872f269ef0dcad8b1641e8c
horace <horce1024@gmail.com>
2023-05-29 22:53:22 +0800

    每日一题（用优雅一点的解法）

---
+++ Solution1455.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    internal class Solution1455
+    {
+        public int AverageValue(int[] nums)
+        {
+            IEnumerable<int> enumerable = from num in nums where num % 2 == 0 && num % 3 == 0 select num;
+            int count = enumerable.Count();
+            if (count == 0) { return 0; }
+            int result = enumerable.Sum();
+            return result / count;
+        }
+    }
+}
