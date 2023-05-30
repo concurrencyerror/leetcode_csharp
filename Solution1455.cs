85b6173353efd4b524485df0a196a7a94351ab43
horace <horce1024@gmail.com>
2023-05-30 08:54:21 +0800

    改一版，不是特别熟悉c#

--- Solution1455.cs
+++ Solution1455.cs
@@ -11,10 +11,13 @@ namespace leetcode
     {
         public int AverageValue(int[] nums)
         {
-            IEnumerable<int> enumerable = from num in nums where num % 2 == 0 && num % 3 == 0 select num;
-            int count = enumerable.Count();
-            if (count == 0) { return 0; }
-            int result = enumerable.Sum();
+            var list = (from num in nums where num % 2 == 0 && num % 3 == 0 select num).ToList();
+            int count = list.Count();
+            if (count == 0)
+            {
+                return 0;
+            }
+            int result = list.Sum();
             return result / count;
         }
     }
