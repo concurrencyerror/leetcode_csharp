29f0408ff3972864abef305ce9237aa85cbb477d
horace <horce1024@gmail.com>
2023-06-05 12:59:57 +0800

    每日一题

---
+++ Solution2460.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode
+{
+    internal class Solution2460
+    {
+        public int[] ApplyOperations(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    nums[i] = nums[i] * 2;
+                    nums[i + 1] = 0;
+                }
+            }
+            IEnumerable<int> enumerable = from num in nums where num != 0 select num;
+            int size = enumerable.Count();
+            return enumerable.Concat(Enumerable.Repeat(0, nums.Length - size)).ToArray();
+        }
+    }
+}
