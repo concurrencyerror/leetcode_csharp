ec95c17925d1be82432566aa2099a825cad2e9e7
Horace <horce1024@gmail.com>
2023-09-15 09:59:31 +0800

    每日一题

---
+++ Solution51.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    internal class Solution50
+    {
+        public int GiveGem(int[] gem, int[][] operations)
+        {
+            for (int i = 0; i < operations.Length; i++)
+            {
+                int v = gem[operations[i][0]] / 2;
+                gem[operations[i][0]] -= v;
+                gem[operations[i][1]] += v;
+            }
+            return gem.Max() - gem.Min();
+        }
+    }
+}
