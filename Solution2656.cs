0595f606477c1fe2607c261a0deff820ea12f334
Horace <horce1024@gmail.com>
2023-11-15 09:02:35 +0800

    每日一题

---
+++ Solution2656.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    internal class Solution2656
+    {
+        public int MaximizeSum(int[] nums, int k)
+        {
+            int max = nums.Max();
+            int result = 0;
+            for (int i = 0; i < k; i++)
+            {
+                result += max;
+                max++;
+            }
+            return result;
+        }
+    }
+}
