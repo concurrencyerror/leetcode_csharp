544b278f956dfc75affc4bf1ff709fa226c3f7c6
Horace <horce1024@gmail.com>
2023-12-07 16:34:52 +0800

    加入测试的框架，写code wars

---
+++ codewar/Disemvowel .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace leetcode.codewar
+{
+    public static class DisemvowelImpl
+    {
+        public static string Disemvowel(string str)
+        {
+            /*string[] strings = str.Split(' ');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string s = strings[i];
+                string v = Regex.Replace(s, "[aeiouAEIOU]", "");
+                strings[i] = v;
+            }
+            return string.Join(" ", strings);*/
+            return Regex.Replace(str, "[aeiou]", "", RegexOptions.IgnoreCase);
+        }
+    }
+}
