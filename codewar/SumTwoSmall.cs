0e250557917dff3fdc46aaec9a751f3088f4dfc9
Horace <horce1024@gmail.com>
2023-12-08 11:12:00 +0800

    每日一题

---
+++ codewar/SumTwoSmall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.codewar
+{
+    public static class SumTwoSmall
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            /*Array.Sort(numbers);
+            return numbers[0] + numbers[1];*/
+            return numbers.OrderBy(x => x).Take(2).Sum();
+        }
+    }
+}
