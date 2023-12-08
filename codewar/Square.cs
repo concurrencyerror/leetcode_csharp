dfa654245211abc1b4a2d2c72e9bf8952826e487
Horace <horce1024@gmail.com>
2023-12-08 08:51:09 +0800

    每日一题

---
+++ codewar/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.codewar
+{
+    public static class Square
+    {
+        public static int SquareDigits(int n)
+        {
+            if (n == 0) return 0;
+            string result = "";
+            while (n > 0)
+            {
+                int item = n % 10;
+                result = Math.Pow(item, 2) + result;
+                n /= 10;
+            }
+            return int.Parse(result);
+        }
+    }
+}
