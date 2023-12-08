5c5816b80cd7047e2042486a8b3e8606d3ff42d5
Horace <horce1024@gmail.com>
2023-12-08 17:03:09 +0800

    每日一题（回家调试）

---
+++ codewar/FindMissLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.codewar
+{
+    public static class FindMissLetter
+    {
+        public static char FindMissingLetter(char[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != array[0]+i) {
+                    return array[i];
+                }
+            }
+            return '_';
+        }
+    }
+}
