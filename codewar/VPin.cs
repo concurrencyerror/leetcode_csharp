82250be8d9d911fd3116bb7bc2239340b09df0e0
Horace <horce1024@gmail.com>
2023-12-08 14:12:11 +0800

    每日一题

---
+++ codewar/VPin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace leetcode.codewar
+{
+    public static class VPin
+    {
+        public static bool ValidatePin(string pin)
+        {
+            if (pin.Length == 4 || pin.Length == 6)
+            {
+                return Regex.IsMatch(pin, "^\\d+\\z");
+            }
+            return false;
+        }
+    }
+}
